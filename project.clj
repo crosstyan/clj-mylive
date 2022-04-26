2adb541b7ff74e383e65b0251d0b697292d6724f
crosstyan <crosstyan@outlook.com>
2022-04-26 16:47:49 +0800

    remove idea folder

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.match "1.0.0"]
                  [org.clojure/core.async "1.5.648"]
-                 [aleph "0.4.6"]
+                 [aleph "0.4.7"]
                  [com.longyb/mylive "0.0.1"]]
   :repl-options {:init-ns elevator-server.core})
