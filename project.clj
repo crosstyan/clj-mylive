5dedf7566259d61bf54655da04d180783d9f9c87
crosstyan <crosstyan@outlook.com>
2022-04-28 21:12:14 +0800

    dump dependency netty

--- project.clj
+++ project.clj
@@ -6,13 +6,20 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.match "1.0.0"]
                  [org.clojure/core.async "1.5.648"]
-                 [aleph "0.4.7"]
-                 [com.longyb/mylive "0.0.1" :exclusions [io.netty/netty-all
-                                                         ch.qos.logback/logback-classic]]
+                 [io.netty/netty-all "4.1.76.Final"]
+                 [aleph "0.4.7" :exclusions [org.clojure/tools.logging
+                                             io.netty/netty-transport
+                                             io.netty/netty-transport-native-epoll
+                                             io.netty/netty-codec
+                                             io.netty/netty-codec-http
+                                             io.netty/netty-handler
+                                             io.netty/netty-handler-proxy
+                                             io.netty/netty-resolver]]
+                 [com.longyb/mylive "0.0.1" :exclusions [io.netty/netty-all]]
                  [metosin/reitit "0.5.18"]
                  [org.clojure/spec.alpha "0.3.218"]
                  [metosin/muuntaja "0.6.8"]
-                 [io.aviso/pretty "1.1.1"]
-                 [ring "1.9.0"]
-                 [com.novemberain/monger "3.1.0"]]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [ring "1.9.5"]
+                 [com.novemberain/monger "3.5.0"]]
   :repl-options {:init-ns elevator-server.core})
